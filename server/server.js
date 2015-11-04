bba6f41329527113fb26bd0c9e299d63ff35db1c
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-11-04 11:38:47 -0800

    fix server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '../client'));
+var PORT = process.env.PORT || 8000;
 
-app.listen(8000);
+app.use(express.static(__dirname + '/../client'));
+
+var server = app.listen(PORT, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Wordpin listening at http://', host, port);
+});
 
 module.exports = app;
